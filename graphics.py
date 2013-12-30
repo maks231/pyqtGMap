6950f4a4ff6b51a7b866f2d78c2a8be9781b80ce
Maxim A <maxim231@rambler.ru>
2013-12-30 10:23:40 +0200

    Graphics inherits QtGui.QGraphicsScene and TileUtils

--- graphics.py
+++ graphics.py
@@ -7,36 +7,33 @@ from defines import *
 from utils import *
 from math import *
 
-class Graphics(QtGui.QWidget, TileUtils):
+class Graphics(QtGui.QGraphicsScene, TileUtils):
     
     def __init__(self, width, height):
         
         TileUtils.__init__(self)
-        
-        QtGui.QWidget.__init__(self) # for sigmals an slots operations
+        QtGui.QGraphicsScene.__init__(self) # for signals and slots operations
         
         self.pic = QtGui.QPixmap(TILESIZE_XY[0], TILESIZE_XY[1])
         self.noImage = QtGui.QPixmap()   # an image "there is an error or no data"
         self.noImage.load("./icons/missing.png")  # loads the image from the given full path
-        
-        #width and hight of the scene
-        self.width = width
-        self.height = height
-        
-        self.scene = QtGui.QGraphicsScene(0, 0, width, height) #new scene
-        self.scene.setBackgroundBrush(QtCore.Qt.black)
-        
+            
+        self.setSceneRect(0, 0, width, height)
+               
         self.mainPixmap = QtGui.QPixmap(width, height)
         self.mainItem = QtGui.QGraphicsPixmapItem();
         self.mainItem.setPixmap(self.mainPixmap)
-
-        self.scene.addItem(self.mainItem)
-
+        
+        self.addItem(self.mainItem)
+        
         self.updateTimer = QtCore.QTimer(self)
         self.updateTimer.setInterval(UPDATE_GRAPHIC_RATE)
         self.updateTimer.timeout.connect(self.updateScene)
         self.updateTimer.start()
 
+        self.offsetFromCenter = [0, 0]
+        self.initialOffsetFromCenter = [0, 0]
+        
         self.centralTileID = [50, 80]
         self.center = XY_PIX_COORD
         self.tilesNumbXY = XY_PIX_COORD
@@ -61,8 +58,8 @@ class Graphics(QtGui.QWidget, TileUtils):
                 self.pushFront(tile) #adds a tile to tiles list
               
     def setCenter(self):
-        self.center = self.scene.width()/2, self.scene.height()/2 # the center of view area
-        self.tilesNumbXY = int(ceil(self.scene.width()/TILESIZE_XY[0]))+1, int(ceil(self.scene.height()/TILESIZE_XY[1]))+1 # number of tiles along X and Y axes
+        self.center = self.width()/2, self.height()/2 # the center of view area
+        self.tilesNumbXY = int(ceil(self.width()/TILESIZE_XY[0]))+1, int(ceil(self.height()/TILESIZE_XY[1]))+1 # number of tiles along X and Y axes
         
     def update (self):
         
@@ -79,8 +76,8 @@ class Graphics(QtGui.QWidget, TileUtils):
                 
                 tempTileID = [tileID[0] + x, tileID[1] + y, 9]
                 #     (offcet from the center in pixels) - offcet id from the current centeral tile  *  x size of a tile
-                xx = (self.center[0] - HALFTILESIZE_XY[0]) + ((tempTileID[0] - self.centralTileID[0]) * TILESIZE_XY[0])
-                yy = (self.center[1] - HALFTILESIZE_XY[1]) + ((tempTileID[1] - self.centralTileID[1]) * TILESIZE_XY[1])
+                xx = (self.center[0] - HALFTILESIZE_XY[0]) + ((tempTileID[0] - self.centralTileID[0]) * TILESIZE_XY[0]) + self.offsetFromCenter[0]
+                yy = (self.center[1] - HALFTILESIZE_XY[1]) + ((tempTileID[1] - self.centralTileID[1]) * TILESIZE_XY[1]) + self.offsetFromCenter[1]
                
                 index = self.getTileIndexInMapArray(self.tilesMap, tempTileID)
                 
@@ -109,15 +106,39 @@ class Graphics(QtGui.QWidget, TileUtils):
         self.mainItem.setPixmap(self.mainPixmap);
         
     def getScene(self):        #returns pointer on the QGraphicsScene 
-        return self.scene
+        return self
     
-    
-    def moveEvent ( event ):
-        print "event"
+    def mousePressEvent(self, event):
+        
+        self.initialOffsetFromCenter = event.scenePos().x(), event.scenePos().y()
+       # self.update()
        
+    def mouseMoveEvent(self, event):
+        
+        position = event.scenePos().x(), event.scenePos().y()
+        
+        self.offsetFromCenter[0] = position[0] - self.initialOffsetFromCenter[0]   
+        self.offsetFromCenter[1] = position[1] - self.initialOffsetFromCenter[1]
+        self.update()
     
+    def mouseReleaseEvent(self, event):
+        
+#        idX = ceil(self.offsetFromCenter[0] % HALFTILESIZE_XY[0])
+#        if idX >= 0:
+#            self.centralTileID[0] += idX
+#        else:
+#            self.centralTileID[0] -= idX
+            
+#        idY = ceil(self.offsetFromCenter[1] % (HALFTILESIZE_XY[1]))
+#        if idY >= 0:
+#            self.centralTileID[1]+=idY
+#        else:
+#            self.centralTileID[1]-=idY
+        
+        self.update()
+        
     def setViewportSize(self, width, height):
-        self.scene.setSceneRect(0, 0, width, height)
+        self.setSceneRect(0, 0, width, height)
         self.setCenter()
         self.mainPixmap = QtGui.QPixmap(width, height)
         self.update()
--- gui.py
+++ gui.py
@@ -14,7 +14,8 @@ class MainWindow(QtGui.QMainWindow):
       
         self.graphics = Graphics(TILESIZE_XY[0], TILESIZE_XY[1])
         self.ui.graphicsView.setScene(self.graphics.getScene())
-       # self.ui.graphicsView.setViewport(QtOpenGL.QGLWidget())   
+        self.ui.graphicsView.setViewport(QtOpenGL.QGLWidget())   
    
     def resizeEvent(self, event):
        self.graphics.setViewportSize(self.ui.graphicsView.width()-2, self.ui.graphicsView.height()-2)
+    
